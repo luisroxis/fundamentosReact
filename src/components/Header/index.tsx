05d7ee52deecbe8e2a955c8a4a24d1c7beb48a1b
Luis Carlos <luiscroxis@gmail.com>
2020-11-06 20:09:11 -0300

    feat: desafio 7

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Link, useRouteMatch } from 'react-router-dom';
+
+import { Container } from './styles';
+
+import Logo from '../../assets/logo.svg';
+
+interface HeaderProps {
+  size?: 'small' | 'large';
+}
+
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => {
+  const { path } = useRouteMatch();
+  return (
+    <Container size={size}>
+      <header>
+        <img src={Logo} alt="GoFinances" />
+        <nav>
+          <Link to="/" className={path === '/' ? 'active' : ''}>
+            Listagem
+          </Link>
+          <Link to="/import" className={path === '/import' ? 'active' : ''}>
+            Importar
+          </Link>
+        </nav>
+      </header>
+    </Container>
+  );
+};
+
+export default Header;
